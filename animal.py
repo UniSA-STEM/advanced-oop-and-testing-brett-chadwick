cb003e7cc859fcc7e683226c318b23491fe52593
{Brett Chadwick} <[chadwick081601@gmail.com]>
2025-11-24 06:00:50 +1100

    Updated description.

--- animal.py
+++ animal.py
@@ -1,8 +1,8 @@
 '''
-File: filename.py
+File: animal.py
 Description: A brief description of this Python module.
-Author: Billy Bizilis
-ID: 110100110
-Username: bizvy001
+Author: Brett Chadwick
+ID: 110407073
+Username: chabx001
 This is my own work as defined by the University's Academic Integrity Policy.
 ''
