4cbb50293d51b1e0575444dd43806f6752e639fb
{Brett Chadwick} <[chadwick081601@gmail.com]>
2025-11-24 10:39:12 +1100

    Updated description.

--- staff.py
+++ staff.py
@@ -5,4 +5,12 @@ Author: Brett Chadwick
 ID: 110407073
 Username: chabx001
 This is my own work as defined by the University's Academic Integrity Policy.
-''
+'''
+class Staff:
+    """Declares the staff class attributes, uses staffID to display potential duplicate names."""
+    def __init__(self, staffID, name, job, responsibilities):
+        self.staffID = staffID
+        self.name = name
+        self.job = job
+        self.responsibilities = responsibilities
+
